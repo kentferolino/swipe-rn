74da4638468f05f06f737fa9f3cd712a06085157
Kent Ferolino <kferolino@mersoft.com>
2020-07-13 14:38:38 +0800

    Forcing swipes

--- src/components/Deck.js
+++ src/components/Deck.js
@@ -8,6 +8,8 @@ import {
 } from "react-native";
 
 const SCREEN_WIDTH = Dimensions.get("window").width;
+const SWIPE_THRESHOLD = 0.25 * SCREEN_WIDTH;
+const SWIPE_OUT_DURATION = 250;
 
 const Deck = ({ data, renderCard }) => {
   const position = new Animated.ValueXY(0, 0);
@@ -18,13 +20,32 @@ const Deck = ({ data, renderCard }) => {
     }).start();
   };
 
+  const forceSwipe = direction => {
+    const x = direction === "right" ? SCREEN_WIDTH : -SCREEN_WIDTH;
+    Animated.timing(position, {
+      toValue: {
+        x,
+        y: 0
+      },
+      duration: SWIPE_OUT_DURATION
+    }).start();
+  };
+
   const panResponder = PanResponder.create({
     onStartShouldSetPanResponder: () => true,
     onPanResponderMove: (event, gesture) => {
       position.setValue({ x: gesture.dx, y: gesture.dy });
     },
-    onPanResponderRelease: () => {
-      resetPosition();
+    onPanResponderRelease: (event, gesture) => {
+      if (gesture.dx > SWIPE_THRESHOLD) {
+        console.log("swipe right");
+        forceSwipe("right");
+      } else if (gesture.dx < -SWIPE_THRESHOLD) {
+        console.log("swipe left");
+        forceSwipe("left");
+      } else {
+        resetPosition();
+      }
     }
   });
 
